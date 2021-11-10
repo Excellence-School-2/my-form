3ec5c192a839ec4c46af8ba53b5cc7dec941a28f
Mellgood <carlocentos@gmail.com>
2021-11-10 10:32:09 +0100

    added view

--- src/main/java/it/ntt/myform/controller/BookController.java
+++ src/main/java/it/ntt/myform/controller/BookController.java
@@ -8,9 +8,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import it.ntt.myform.model.Book;
 import it.ntt.myform.repository.BookRepository;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
